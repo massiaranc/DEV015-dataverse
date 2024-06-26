e9b7907c40fbbf0d7e81a3f43b6474ac07d7ab46
Jessika Sanchez <jessikasanchez@MacBook-Pro-de-Jessika.local>
2024-06-26 11:17:26 -0500

    completa el hito 2

--- src/main.js
+++ src/main.js
@@ -4,3 +4,11 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
+
+const ul = renderItems(data);
+
+const container = document.querySelector('#root');
+
+if (container) {container.appendChild(ul);console.log('Datos renderizados en el DOM');
+} else {console.error('No se encontró el contenedor con id "root".');
+}
